34943dd1a04e56c6c0a80ffd5dd15dac477db9e2
iamchahu <chathurangacithrasena@gmail.com>
2020-07-20 15:48:10 +0530

    initial todo list made in vuex

---
+++ vuex-playlist/src/components/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todo-list-item">
+    <div class="title">
+      <span v-bind:class="{completed: todo.completed}" v-on:click="toggleTodoStatus">{{ todo.title }}</span>
+    </div>
+      <button v-on:click="deleteTodo">Dlete</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["todo"],
+
+    methods: {
+      deleteTodo(){
+        this.$store.dispatch('deleteTodo', this.todo);
+      },
+      toggleTodoStatus(){
+        this.$store.dispatch('toggleTodoStatus', this.todo);
+      }
+    }
+}
+</script>
+
+<style scoped>
+.todo-list-item{
+  width: 250px;
+  margin: 5px auto;
+}
+.title{
+  display: flex;
+  justify-content: space-between;
+}
+.completed{
+  text-decoration: line-through;
+}
+</style>
+
---
+++ vuex-playlist/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+     <todo v-for="todo in todos" v-bind:key="todo.id" v-bind:todo="todo"/>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo'
+export default {
+    components: {
+        Todo
+    },
+    computed: {
+        todos(){
+            return this.$store.state.todos;
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
